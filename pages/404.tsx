77ca19f752107bd2c68aee9207b1373492792390
d_flex <hiyoo95@gmail.com>
2022-09-09 00:41:43 +0900

    Add 404 error page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <h1>
+      This is an invalid access!
+      <style jsx>{`
+        h1 {
+          text-align: center;
+        }
+      `}</style>
+    </h1>
+  );
+}
