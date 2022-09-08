d2318720c1a2d0e18e44681a7a8170069aeec522
d_flex <hiyoo95@gmail.com>
2022-09-09 00:35:13 +0900

    mix with React conventional data fetching and CatAll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Seo from '../components/Seo';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 interface IMovieProps {
   adult: boolean;
   backdrop_path: string;
   genre_ids: number[];
-  id: number;
+  id: string;
   original_language: string;
   original_title: string;
   overview: string;
@@ -21,21 +23,43 @@ interface IMovieProps {
 export default function Home({
   results,
 }: InferGetServerSidePropsType<GetServerSideProps>) {
+  const router = useRouter();
+
+  const handleClick = (movie: IMovieProps) => {
+    const { id, title } = movie;
+
+    router.push(`/movies/${title}/${id}`);
+  };
   return (
     <div className='container'>
       <Seo title='Home' />
       {results?.map((movie: IMovieProps) => (
         <div key={movie.id}>
-          <div className='movie' key={movie.id}>
+          <div
+            onClick={() => handleClick(movie)}
+            className='movie'
+            key={movie.id}
+          >
             <img
               src={`https://images.tmdb.org/t/p/w500/${movie.poster_path}`}
               alt={movie.original_title}
             />
-            <h4>{movie.original_title}</h4>
+            <h4>
+              <Link
+                href={`movies/${movie.original_title}/${movie.id}`}
+                key={movie.id}
+              >
+                <a>{movie.original_title}</a>
+              </Link>
+            </h4>
           </div>
         </div>
       ))}
       <style jsx>{`
+        a {
+          text-decoration: none;
+          color: black;
+        }
         .container {
           display: grid;
           grid-template-columns: repeat(4, 1fr);
@@ -48,6 +72,7 @@ export default function Home({
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          cursor: pointer;
         }
 
         .movie img {
@@ -57,7 +82,7 @@ export default function Home({
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
         .movie:hover img {
-          transform: scale(1.05) translateY(-10px);
+          transform: scale(1.05) translateY(-5px);
         }
         .movie h4 {
           font-size: 18px;
--- pages/movies/[...params].tsx
+++ pages/movies/[...params].tsx
@@ -1,16 +1,38 @@
 import { useRouter, NextRouter } from 'next/router';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { useState, useEffect } from 'react';
 
 export default function Detail({
   params,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router: NextRouter = useRouter();
 
+  const [detail, setDetail] = useState({});
+
   const [title, id] = params || [];
+
+  useEffect(() => {
+    const getMovieDetail = async () => {
+      const response = await (await fetch(`/api/movies/${id}`)).json();
+      setDetail(response);
+    };
+    getMovieDetail();
+  }, []);
 
   return (
     <div>
-      <h2>{title}</h2>
+      <h2>
+        {title}{' '}
+        {title !== detail.original_title ? (
+          <span>({detail.original_title})</span>
+        ) : null}
+      </h2>
+      <img
+        src={`https://images.tmdb.org/t/p/w500/${detail.poster_path}`}
+        alt={detail.original_title}
+      />
+      <p>released in: {detail.release_date}</p>
+
       <style jsx>{`
         div {
           display: flex;
@@ -32,6 +54,8 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const {
     params: { params },
   } = ctx;
+
+  console.log(ctx.params);
   return {
     props: {
       params,
