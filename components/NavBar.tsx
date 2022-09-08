b140385b9ad1ea9108cf6f34788db7e33b73fb01
d_flex <hiyoo95@gmail.com>
2022-09-08 17:41:30 +0900

    apply global setting with _app.ts

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,20 +2,26 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function NavBar() {
+  const router = useRouter();
   return (
     <nav>
       <Link href='/'>
-        <a>Home</a>
+        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
       </Link>
       <Link href='/about'>
-        <a>About</a>
+        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
       </Link>
       <style jsx>{`
         nav {
-          background-color: green;
+          display: flex;
+          justify-content: space-between;
         }
         a {
           text-decoration: none;
+        }
+
+        .active {
+          color: red;
         }
       `}</style>
     </nav>
---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+import NavBar from '../components/NavBar';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>
+        {``}
+      </style>
+    </>
+  );
+}
